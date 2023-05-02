2dfd6db99b4fa91e577e2ec050d61ad7d7f1f53b
prerna bhardwaj <109895343+prernabharadwaj-Code@users.noreply.github.com>
2023-05-02 09:48:57 +0530

    Create fibonaaciseries.c

---
+++ fibonaaciseries.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main ()
+{
+    int n1=0,n2=1,i,n3, n ;
+    printf("enter the number of elements ");
+    scanf("%d",&n);
+    printf("     %d",n1);
+     printf(" \n %d",n2);
+    for(i=2;i<n;i++)
+    {
+        n3=n1+n2;
+        printf("\n %d",n3);
+        n1=n2;
+        n2=n3;
+        
+    }
+    return 0 ;
+    
+    
